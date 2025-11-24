86da49ee4cce60978c27312acb2f68d5a973bfdc
Kai-330 <tankai1125@gmail.com>
2025-11-24 20:03:27 +0800

    Add files via upload

---
+++ Practical 5/Q1.py
@@ -0,0 +1,11 @@
+message = input("Enter a string:").strip()
+
+words = message.split()
+
+acyronm = ""
+
+for word in words:
+    acyronm += word[0].upper()
+
+print("The acronym for '{}'is '{}'".format(message.strip(),acyronm))      
+      
